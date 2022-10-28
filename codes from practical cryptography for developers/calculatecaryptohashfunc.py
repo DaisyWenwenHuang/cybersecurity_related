3044a38d7d3d6ba15ad49e7b4c242114e9712628
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-10-28 20:29:28 +0000

    update

--- codes from practical cryptography for developers/calculatecaryptohashfunc.py
+++ codes from practical cryptography for developers/calculatecaryptohashfunc.py
@@ -1,2 +1,18 @@
 # https://cryptobook.nakov.com/cryptographic-hash-functions/hash-functions-examples
 # practice code from link above
+import hashlib, binascii
+
+text = 'hello'
+data = text.encode("utf8")
+
+sha256hash = hashlib.sha256(data).digest()
+print("SHA-256:   ", binascii.hexlify(sha256hash))
+
+sha3_256 = hashlib.sha3_256(data).digest()
+print("SHA3-256:  ", binascii.hexlify(sha3_256))
+
+blake2s = hashlib.new('blake2s', data).digest()
+print("BLAKE2s:   ", binascii.hexlify(blake2s))
+
+ripemd160 = hashlib.new('ripemd160', data).digest()
+print("RIPEMD-160:", binascii.hexlify(ripemd160))
