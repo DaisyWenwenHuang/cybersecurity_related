36391cc16b83b0f5ef0332c232b5a3ffed0243b0
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-11-03 16:20:04 +0000

    update

--- codes from practical cryptography for developers/calculatecaryptohashfunc.py
+++ codes from practical cryptography for developers/calculatecaryptohashfunc.py
@@ -1,6 +1,6 @@
 # https://cryptobook.nakov.com/cryptographic-hash-functions/hash-functions-examples
 # practice code from link above
-# Using R 
+# Using python
 import hashlib, binascii
 
 text = 'hello'
